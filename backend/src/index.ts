5cfc941f7adccf8d76fb3a864f55985a165cc7f9
Huntertk <taufik.khan65@gmail.com>
2024-04-29 23:58:37 +0530

    Initally Backend Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/test', (req:Request, res:Response) => {
+    
+    res.json({
+        message:"Hello"
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is Running on PORT 3000");
+    
+})
