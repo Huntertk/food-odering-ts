687289daee012c4802491282e24b8f9505486979
Huntertk <taufik.khan65@gmail.com>
2024-04-30 12:55:53 +0530

    Data base connected to application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Db is connected")).catch((err) => console.log(err))
 
 const app = express();
+const PORT = process.env.PORT as string || 4000
 
 //Middleware
 app.use(express.json());
@@ -17,7 +20,7 @@ app.get('/test', (req:Request, res:Response) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Server is Running on PORT 3000");
+app.listen(PORT, () => {
+    console.log("Server is Running on PORT ", PORT);
     
 })
