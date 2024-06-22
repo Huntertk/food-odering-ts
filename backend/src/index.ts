551683d8b07ea020fa97f7ae1dc564d04f67b28a
Huntertk <taufik.khan65@gmail.com>
2024-06-22 10:53:03 +0530

    basic express app setup and listing port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Log");
+import express, {Request, Response} from "express";
+import cors from 'cors';
+import 'dotenv/config';
+
+
+//Initializing Express App
+const app = express();
+
+
+//Middlwares
+app.use(express.json());
+app.use(cors())
+
+
+//Routes
+app.get('/test', async (req:Request, res:Response) => {
+    res.status(200).json({message:"Hello From Test"})
+})
+
+//Server Listining
+
+app.listen(3000, () => {
+    console.log(`Server is running on PORT : 3000`);
+})
