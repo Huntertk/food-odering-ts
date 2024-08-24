65ee315a51984d9853ba5f23e123518b3a3c7541
Huntertk <taufik.khan65@gmail.com>
2024-08-24 17:09:30 +0530

    Layout Component Created

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<span>Home Page</span>}/>
+        <Route path="/" element={
+          <Layout>
+            <span>Home Page</span>
+          </Layout>
+        }
+          />
         <Route path="/user-profile" element={<span>USER PROFILE Page</span>}/>
         <Route path="*" element={<Navigate to="/" />}/>
     </Routes>
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from '@/components/Header'
+
+type PropsType = {
+    children:React.ReactNode
+}
+
+const Layout = ({children}:PropsType) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="container mx-auto flex-1 py-10">
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Layout
