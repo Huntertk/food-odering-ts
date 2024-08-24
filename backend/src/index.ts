da18df249f511ce757caab990e9f988e38b5612d
Huntertk <taufik.khan65@gmail.com>
2024-08-24 14:03:31 +0530

    previos code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express";
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from "mongoose";
-
+import myUserRoute from './routes/myUserRoute'
 
 //Database connection
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -20,9 +20,7 @@ app.use(cors())
 
 
 //Routes
-app.get('/test', async (req:Request, res:Response) => {
-    res.status(200).json({message:"Hello From Test"})
-})
+app.use("/api/my/users", myUserRoute)
 
 //Server Listining
 
