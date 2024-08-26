17060ffe7c6af288d07d32aa49573947e7e172f7
Huntertk <taufik.khan65@gmail.com>
2024-08-26 15:30:49 +0530

    auth login and logout completed

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,16 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { Button } from "./ui/button"
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
-  const {loginWithRedirect} = useAuth0();
+  const {loginWithRedirect, isAuthenticated} = useAuth0();
   return (
-    <Button 
-    className="font-bold hover:text-orange-500 hover:bg-white" 
-    onClick={async () => await loginWithRedirect()}
-    variant={"ghost"} 
-    >
-        Login
-    </Button>
+    <span className="flex space-x-2 items-center">
+        {
+          isAuthenticated ? (
+            <UsernameMenu />
+          ) : (
+            <Button 
+            className="font-bold hover:text-orange-500 hover:bg-white" 
+            onClick={async () => await loginWithRedirect()}
+            variant={"ghost"} 
+            >
+                Login
+            </Button>
+          )
+        }
+    </span>
   )
 }
 
